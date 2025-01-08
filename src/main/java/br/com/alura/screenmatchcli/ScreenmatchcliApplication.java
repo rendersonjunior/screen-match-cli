66a40358feb040eccde3fbd92b8055c713373b69
Renderson Junior <rendersonjuniorcmp@gmail.com>
2025-01-08 18:09:22 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/br/com/alura/screenmatchcli/ScreenmatchcliApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/screenmatchcli/ScreenmatchcliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
